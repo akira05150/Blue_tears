e5807a343dfeb56c095383aa1046aeefd38b1968
Joann <59599987+joannhsiao@users.noreply.github.com>
2022-11-08 14:25:47 +0800

    v1

---
+++ distance.py
@@ -0,0 +1,41 @@
+import cv2
+
+# distance from camera to object(face) measured
+KNOWN_DISTANCE = 60  # centimeter
+FACE_WIDTH = 14  # centimeter
+# Colors
+GREEN = (0, 255, 0)
+RED = (0, 0, 255)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+fonts = cv2.FONT_HERSHEY_COMPLEX
+
+"""
+# Define the codec and create VideoWriter object
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+out = cv2.VideoWriter("output21.mp4", fourcc, 30.0, (640, 480))
+"""
+
+# face detector object
+face_detector = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# focal length finder function
+def focal_length(measured_distance, real_width, width_in_rf_image):
+    focal_length_value = (width_in_rf_image * measured_distance) / real_width
+    return focal_length_value
+
+# distance estimation function
+def distance_finder(focal_length, real_face_width, face_width_in_frame):
+    distance = (real_face_width * focal_length) / face_width_in_frame
+    return distance
+
+# face detector function
+def face_data(image):
+    face_width = 0
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_detector.detectMultiScale(gray_image, 1.3, 5)
+    for (x, y, h, w) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), WHITE, 1)
+        face_width = w
+
+    return face_width
---
+++ main.py
@@ -0,0 +1,17 @@
+from app import *
+#from flask import Flask, render_template, request
+"""
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+"""
+def main():
+    detect_hands_gesture()
+
+if __name__ == '__main__':
+	#port = int(os.environ.get('PORT', 5000))
+    #app.run(host='0.0.0.0', port=port)
+    main()
+    
