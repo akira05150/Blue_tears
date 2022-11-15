20cab50e5ad170a5afeeb3b99a62a30a8784faf1
akira05150 <hkn8738@gmail.com>
2022-11-15 19:25:12 +0800

    fix: put images to static/images/

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 from detect_main import *
-import cv2
+# import cv2
 import os
-#from flask import Flask, render_template, request
+from flask import Flask, render_template, request
 
-#app = Flask(__name__)
-"""
+app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
-"""
+
 def main():
     detect_main()
 
 if __name__ == '__main__':
-    #port = int(os.environ.get('PORT', 5000))
-    #app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
     main()
     
