1ad2fb63b174046f4823f77796a1b78121e3ad17
Joann <59599987+joannhsiao@users.noreply.github.com>
2022-11-21 22:55:40 +0800

    v4 link opencv with website

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from detection import *
 from flask import Flask, render_template, Response
-from time import sleep
 
 app = Flask(__name__)
 
@@ -13,7 +12,6 @@ def detect():
         frame = get_frame()
         yield (b'--frame\r\n'
         b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        sleep(0.01)
 
 @app.route("/video_feed", methods=['GET'])
 def video_feed():
